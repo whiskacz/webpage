b618cc04ba4b67fcac2c860cdad67c86e94b91d7
whiskacz <pawelblasiak1985@gmail.com>
2024-01-03 23:01:13 +0100

    paralax in progress ...still

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,28 +1,11 @@
-import { motion } from 'framer-motion';
-import starsBackground from './starsBackground.jpg'; // Zaimportowanie obrazu
+import './main.scss'
 
 const Main = () => {
   return (
-    <motion.div
-      className="background"
-      style={{
-        backgroundImage: `url(${starsBackground})`, // Użycie zaimportowanego obrazu
-        backgroundPosition: 'center',
-        width: '100vw',
-        height: '100vh',
-        position: 'absolute',
-        overflow: 'hidden',
-      }}
-      animate={{
-        x: [0, 100, 0, -100, 0], // Animacja po osi X
-        y: [0, 50, 100, 50, 0], // Animacja po osi Y
-      }}
-      transition={{
-        duration: 5, // Czas trwania animacji w sekundach
-        ease: 'linear', // Wybrany rodzaj animacji - tutaj liniowy
-        loop: Infinity, // Powtarzanie w nieskończoność
-      }}
-    />
+    <>
+        <div className='background'></div>
+        <div className="mountain"></div>
+    </>
   );
 }
 
