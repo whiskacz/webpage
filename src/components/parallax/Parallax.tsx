52fe95a0205b1aaff725bb10766d185281e1eff6
whiskacz <pawelblasiak1985@gmail.com>
2024-01-17 22:36:56 +0100

    framer motion scale  in progress

--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -1,32 +1,42 @@
-import { motion, useTransform, useScroll } from 'framer-motion';
-import { useRef } from 'react';
+import { motion, useScroll } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 import './parallax.scss';
 
 function Parallax() {
 
-  const ref = useRef()
+  let scrollYProgressValue: number = 0
+  const ref = useRef<HTMLDivElement>(null);
   const {scrollYProgress} = useScroll({
     target:ref,
-    offset:["start start", "end start"]
+    offset:["start start", "end end"]
   })
+useEffect(() => {
+    // Nasłuchiwanie zmian w scrollYProgress
+    const updateOpacity = (value: number) => {
+      scrollYProgressValue = parseFloat(value.toFixed(2)) + 1
+      console.log(`postęp przewijania ${scrollYProgressValue}`);
+    };
 
-  use
-  const ovalAnimation = useTransform(scrollYProgress, [0,1], ["0%","100%"])
+    const unsubscribe = scrollYProgress.onChange(updateOpacity);
 
-  const numericScrollProgress = ovalAnimation.get();
-  console.log(`scroll progress ${numericScrollProgress}`)
+    // Oczyszczanie nasłuchiwacza przy odmontowywaniu komponentu
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollYProgress]);
+
   return (
-    <div ref={ref} className='parallaxContainer'>
-      <motion.img 
+    <motion.div ref={ref} className='parallaxContainer'>
+      <img 
       src="/src/images/oval.png" 
       alt="ovalPNG" 
       className='backgroundOval element'
-      style={{ transform: `scale(${1 + numericScrollProgress})` }}
+      style={{ transform: `scale(${scrollYProgressValue})` }}
       />
       {/* <img src="/src/images/backgroundMain.jpg" alt="backgroundMain" className='backgroundMain element' />
       <img src="/src/images/mount1.png" alt="mount1" className='mount1 element' />
       <img src="/src/images/mount2.png" alt="mount2" className='mount2 element' /> */}
-    </div>
+    </motion.div>
   )
 }
 
